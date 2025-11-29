1497e172c14e4d156177340e74375e096eacb27f
Hossain Ahmmed <hossainahmmedtaufiq22@gmail.com>
2025-11-29 20:04:52 +0600

    post req

--- CRUD-02/index.js
+++ CRUD-02/index.js
@@ -1,14 +1,26 @@
-import express from 'express'
+import express from "express";
+import mongoose from "mongoose";
 
-const app = express()
+const app = express();
+app.use(express.json());
+// const PORT = process.env.PORT || 3000;
+const MONGO_URL = process.env.MONGO_URL;
 
-// app.get('/', (req, res) => {
-//   res.send('Web server is running correctly')
-// })
+app.post("/api/products", (req, res) => {
+  console.log(req.body);
+  res.send(req.body);
+});
 
-
-app.get('/',(req,res)=>{
-    console.log('Server is running correctly')
-    res.send('Hello form node api')
-})
-app.listen(3000)
+mongoose
+  .connect(
+    "mongodb+srv://hossainahmmedtaufiq22_db_user:yJxvXD5HVjSgmKvk@cluster0.myjowpg.mongodb.net/?appName=Cluster0"
+  )
+  .then(() => {
+    console.log("mongodb is connected");
+    app.listen(3000, () => {
+      console.log("Server is running on port 3000");
+    });
+  })
+  .catch((e) => {
+    console.log(e, "mongodb is not connected");
+  });
---
+++ CRUD-02/model/userModel.js
@@ -0,0 +1,28 @@
+import mongoose from "mongoose";
+
+const productSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, "Please provide a product name"],
+  },
+  quantity: {
+    type: Number,
+    required: [true, "Please provide product quantity"],
+    default: 0,
+  },
+  price: {
+    type: Number,
+    required: [true, "Please provide product price"],
+    default: 0,
+  },
+  image: {
+    type: String,
+    required: false,
+  },
+
+  timestamps: true,
+});
+
+
+const Product = mongoose.model("product", productSchema)
+export default Product;
