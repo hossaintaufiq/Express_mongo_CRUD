436e91c66dae1b802c1947b2b9b9fbaf8691501b
Hossain Ahmmed <hossainahmmedtaufiq22@gmail.com>
2025-11-29 19:12:25 +0600

    postman and gitignore

--- CRUD-02/index.js
+++ CRUD-02/index.js
@@ -2,8 +2,13 @@ import express from 'express'
 
 const app = express()
 
-app.get('/', (req, res) => {
-  res.send('Web server is running correctly')
-})
+// app.get('/', (req, res) => {
+//   res.send('Web server is running correctly')
+// })
 
+
+app.get('/',(req,res)=>{
+    console.log('Server is running correctly')
+    res.send('Hello form node api')
+})
 app.listen(3000)
